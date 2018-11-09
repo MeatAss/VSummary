3bcac538ab8a0afab8e98b485e263fa0d7ceb596
MeatAss <labus_slava@mail.ru>
2018-11-09 14:09:59 +0300

    Create project. Add createSummary.mustache

---
+++ src/main/java/Com/VSummary/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package Com.VSummary.config;
+
+import Com.VSummary.domain.CustomHandshakeHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/", "/queue/");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("gs-websocket")
+                .setHandshakeHandler(new CustomHandshakeHandler())
+                .withSockJS();
+    }
+}
---
+++ src/main/java/Com/VSummary/controller/MainController.java
@@ -0,0 +1,15 @@
+package Com.VSummary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(Map<String, Object> model){
+        return "createSummary";
+    }
+}
